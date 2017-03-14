c448dabddc96789b56acef101fbab84982edf683
rishikant <rksbtp@gmail.com>
2017-03-14 22:09:25 +0530

    added procedure to do arithmetic with complex no

---
+++ ClojureProgramming/complex_arithmetic.clj
@@ -0,0 +1,52 @@
+(defn make-from-real-imag
+  [x y]
+  (list x y))
+
+(defn make-from-mag-ang
+  [r a]
+  (list (* r (Math/cos a)) (* r (Math/sin a))))
+
+(defn square [x] (* x x))
+
+(defn real-part
+  [z]
+  (first z))
+
+(defn imag-part
+  [z]
+  (last z))
+
+(defn magnitude
+  [z]
+  (Math/sqrt (+ (square (real-part z)) (square (imag-part z)))))
+
+(defn angle
+  [z]
+  (Math/atan (/ (imag-part z) (real-part z))))
+
+(defn add-complex
+  [z1 z2]
+  (make-from-real-imag (+ (real-part z1) (real-part z2))
+                       (+ (imag-part z1) (imag-part z2))))
+(defn sub-complex
+  [z1 z2]
+  (make-from-real-imag (- (real-part z1) (real-part z2))
+                       (- (imag-part z1) (imag-part z2))))
+
+(defn mul-complex
+  [z1 z2]
+  (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+                     (+ (angle z1) (angle z2))))
+
+(defn div-complex
+  [z1 z2]
+  (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+                     (- (angle z1) (angle z2))))
+
+;; TEST
+(def z1 (make-from-real-imag 2 3))
+(def z2 (make-from-real-imag 4 5))
+
+(println (add-complex z1 z2))
+(println (mul-complex z1 z2))
+(println (div-complex z1 z2))
